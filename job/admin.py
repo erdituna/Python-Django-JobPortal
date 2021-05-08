17b6986240815d9c82d79895021467a47a982494
erdituna36@gmail.com <erdituna36@gmail.com>
2021-05-08 15:04:26 +0300

    Model Category ve Product ilişkisi

--- job/admin.py
+++ job/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from job.models import Category
+from job.models import Category, Job
+
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title','status']
     list_filter = ['status']
 
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['title','salary','company','location','category']
+    list_filter = ['status','category']
+
 admin.site.register(Category,CategoryAdmin)
+admin.site.register(Job,JobAdmin)
--- job/models.py
+++ job/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+from django.utils.safestring import mark_safe
+
+
 class Category(models.Model):
     STATUS = (
         ('True', 'True'),
@@ -19,4 +22,36 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+
+
+class Job(models.Model):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) #many to one relation with Category
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image=models.ImageField(blank=True,upload_to='images/')
+    salary = models.IntegerField()
+    type = models.CharField(max_length=150)
+    location = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    detail=models.TextField()
+    slug = models.SlugField()
+    status=models.CharField(max_length=10,choices=STATUS)
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
+
+
+    ## method to create a fake table field in read only mode
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
+
 
