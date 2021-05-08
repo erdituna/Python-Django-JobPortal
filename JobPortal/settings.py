dbcf6d89021c3fd5a89578e6465692d2156ccb9e
erdituna36@gmail.com <erdituna36@gmail.com>
2021-05-08 14:03:27 +0300

    Job Category

--- JobPortal/settings.py
+++ JobPortal/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'job.apps.JobConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- JobPortal/urls.py
+++ JobPortal/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('home.urls')),
+    path('job/', include('job.urls')),
     path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
