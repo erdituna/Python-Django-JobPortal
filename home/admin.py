086637956c88a2e5db3d34fd9c288426d709784a
erdituna36@gmail.com <erdituna36@gmail.com>
2021-05-10 14:15:23 +0300

    Contact Form

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from home.models import Setting
+from home.models import Setting,ContactFormMessage
 
 
 class SettingtAdmin(admin.ModelAdmin):
     list_display = ['title','company', 'update_at','status']
 
+class ContactFormMessageAdmin(admin.ModelAdmin):
+    list_display = ['name','subject', 'update_at','status','note']
+    readonly_fields =('name','subject','email','message','ip')
+    list_filter = ['status']
+
+
 admin.site.register(Setting,SettingtAdmin)
+admin.site.register(ContactFormMessage,ContactFormMessageAdmin)
+
