1a10f90dd721dc1df2351d99875bb3034d4cc8a9
erdituna36@gmail.com <erdituna36@gmail.com>
2021-05-12 16:38:06 +0300

    update ve change password işlemleri

---
+++ user/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # ex: /polls/
+    path('', views.index, name='index'),
+    path('update/', views.user_update, name='user_update'),
+    path('password/', views.change_password, name='change_password'),
+
+    # ex: /polls/5/
+    #path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    #path('addcomment/<int:id>', views.addcomment, name='addcomment'),
+    ]
